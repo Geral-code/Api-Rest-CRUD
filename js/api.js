75518952951acf203ff7de64ebc31551e282c65d
Geraldine <geraldine.orellana@gmail.com>
2022-02-07 19:51:30 -0300

    eliminar cliente

--- js/api.js
+++ js/api.js
@@ -53,3 +53,20 @@ export const obtenerCliente = async id => {
     throw error;
   }
 }
+
+//funcion para eliminar un cliente
+
+export const eliminarCliente = async (id) => {
+
+  try {
+    await fetch (`${URL}/${id}`, {
+      method: 'DELETE'
+
+    });
+
+  } catch (error) {
+    throw error;
+    
+  }
+
+}
--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-import { obtenerClientes } from './api.js';
+import { obtenerClientes, eliminarCliente } from './api.js';
 
 (()=> {
   const listado = document.querySelector('#listado-clientes');
   // cuando se carga el documento html
   document.addEventListener('DOMContentLoaded', mostrarClientes);
+
+  //el llamado a la funcion eliminar
+  listado.addEventListener('click', confirmarEliminar)
+
+
   async function mostrarClientes() {
     const clientes = await obtenerClientes();
     console.log(clientes);
@@ -35,6 +40,34 @@ import { obtenerClientes } from './api.js';
     listado.appendChild(row);
 
     })
+  };
+
+  function confirmarEliminar(event) {
+     
+    if(event.target.classList.contains('eliminar')) {
+      //Capturamos el id del cliente
+      
+      const clienteId = parseInt (event.target.dataset.cliente);
+      Swal.fire ({
+        title: '¿Deseas eliminar este registro?',
+        text: "No podras recuperar la informacion de este usuario!",
+        icon : 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor:'#d33' ,
+        confirmButtonText: 'Si, Eliminar'
+
+      }) .then(respuesta => {
+        if(respuesta.isConfirmed) {
+         eliminarCliente(clienteId);
+        }
+
+    });
+
+
+    
   }
+
   }
+}
 )();
